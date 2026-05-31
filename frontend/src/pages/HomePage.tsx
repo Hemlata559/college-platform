afaaccd83c98d5c0a088474ff2d9e930bed8869e
Hemlata Yadav <yadavhemlata559@gmail.com>
2026-05-31 06:50:32 +0530

    Update API URL for deployment compatibility

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -22,8 +22,10 @@ const HomePage = () => {
   useEffect(() => {
     const fetchColleges = async () => {
       try {
+        //changed to work on deploy as well
+        const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5000";
         const response = await fetch(
-          "http://localhost:5000/api/colleges"
+           `${apiUrl}/api/colleges`
         );
 
         const data = await response.json();
